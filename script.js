42b66fe9e52424f643b9bd19061363b155d59227
Inhye Kang <inhye@bu.edu>
2023-06-01 21:29:12 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,40 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "100%";
+    document.getElementById("main").style.marginRight = "250px";
+    }
+    
+    function closeNav() {
+    document.getElementById("mySidebar").style.width = "0%";
+    document.getElementById("main").style.marginRight= "0%";
+}
+    
+function openPage(pageName,elmnt,color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(pageName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+}
+    
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+//top nav sticky
+window.onscroll = function() {myFunction()};
+
+var topnav = document.getElementById("topnav");
+var sticky = topnav.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+    topnav.classList.add("sticky")
+    } else {
+    topnav.classList.remove("sticky");
+    }
+}
